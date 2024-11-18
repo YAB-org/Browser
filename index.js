d95fa07e5ce9aaf26fad63a0bcecb8b70c3e6d99
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-11-18 19:02:47 +0100

    More boilerplate + Setup screen structure

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('node:path');
+const fs = require('node:fs');
+const userDataPath = app.getPath("userData")
 
-const createWindow = () => {
+// Remove menu abr
+Menu.setApplicationMenu(null);
+
+console.log(path.join(userDataPath, 'user-data.json'))
+
+function createWindow() {
+  // Create browser window
   const win = new BrowserWindow({
-    width: 800,
-    height: 600
+    title: 'YAB',
+    show: false,
+    autoHideMenuBar: true,
+    titleBarStyle: 'hidden',
+    titleBarOverlay: true,
+    webPreferences: {
+      sandbox: false,
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
 
-  win.loadFile('pages/index.html')
+  win.maximize();
+
+  // Check if first use
+  if (fs.existsSync(path.join(userDataPath, 'user-data.json'))) {
+    // Open normal browser
+    win.loadFile('pages/index.html');
+  } else {
+    // Setup
+    win.loadFile('pages/setup.html');
+  }
+
+  win.show();
 }
 
 app.whenReady().then(() => {
