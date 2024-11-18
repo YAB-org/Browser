5bfd905572680cac1010b194e89f7ef128e4c8ac
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-11-18 23:17:41 +0100

    Some stuff unfinished

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const userDataPath = app.getPath("userData")
 // Remove menu bar
 Menu.setApplicationMenu(null);
 
-console.log(path.join(userDataPath, 'user-data.json'))
-
 // Create browser window
 function createWindow() {
   const win = new BrowserWindow({
@@ -22,6 +20,7 @@ function createWindow() {
   })
 
   win.maximize();
+  win.setWindowButtonVisibility(false);
 
   // Check if first use
   if (fs.existsSync(path.join(userDataPath, 'user-data.json'))) {
---
+++ media/setup.js
@@ -0,0 +1,3 @@
+document.getElementById('winConMinimize');
+document.getElementById('winConMaximize');
+document.getElementById('winConClose');
