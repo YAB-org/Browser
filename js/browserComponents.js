9bc173957f26d6a9c2c704388e9bd27cb32a59f8
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-12-10 22:00:08 +0100

    Move tabs to custom elemnt

--- js/browserComponents.js
+++ js/browserComponents.js
@@ -43,10 +43,64 @@ class TabManager {
         } else {
             this.ready = true;
 
-            Array.from(document.querySelectorAll('.tab')).forEach(e=>{
+            class TabElement extends HTMLElement {
+                constructor() {
+                  super();
+                }
+                connectedCallback() {
+                    // Shadow dom
+                    let shadow = this.attachShadow({ mode: 'open' });
+                    let div = document.createElement('div');
+                    div.classList.add('tab');
+                    if (this.getAttribute('selected')!==null) div.classList.add('tab-selected');
+                    this.shadowDiv = div;
+                    div.innerHTML = `<style>
+    .tab {
+        display: flex;
+        gap: 10px;
+        align-items: center;
+        width: 200px;
+        height: 28px;
+        border-radius: 7px 7px 3px 3px;
+        transition: border-radius 0.5s;
+        padding-inline: 10px;
+        -webkit-app-region: no-drag;
+        background: var(--bg-tab-disabled);
+    }
+    .tab:hover {
+        cursor:pointer;
+    }
+    .spacer {
+        flex: 1;
+    }
+    .tab-selected {
+        background: var(--bg-tab-active);
+    }
+</style>
+<div class="tab_icon"></div>
+<div class="tab_title">ye so: { title }</div>
+<span class="spacer"></span>
+<div class="tab_close_container">
+    <svg role="button" width="12" height="12" viewBox="0 0 12 12"><polygon fill="currentColor" fill-rule="evenodd" points="11 1.576 6.583 6 11 10.424 10.424 11 6 6.583 1.576 11 1 10.424 5.417 6 1 1.576 1.576 1 6 5.417 10.424 1"></polygon></svg>
+</div>`;
+                    shadow.appendChild(div);
+                }
+                setSelected(stat) {
+                    if (stat) {
+                        this.shadowDiv.classList.add('tab-selected');
+                        this.setAttribute('selected', true);
+                    } else {
+                        this.shadowDiv.classList.remove('tab-selected');
+                        this.removeAttribute('selected');
+                    }
+                }
+            }
+            customElements.define("yab-tab", TabElement);
+
+            Array.from(document.getElementsByTagName('yab-tab')).forEach(e=>{
                 e.onmousedown = function(){
-                    document.querySelector('.tab:not(.tab-disabled)').classList.add('tab-disabled');
-                    e.classList.remove('tab-disabled')
+                    document.querySelector('yab-tab[selected]').setSelected(false);
+                    e.setSelected(true);
                 }
             })
 
