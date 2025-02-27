b018bfd0509abde9cd9177de96046c1c0bf18cb5
LarsIsHere <lars-private@hotmail.com>
2025-02-27 22:47:20 +0100

    stuff

--- js/browserComponents.js
+++ js/browserComponents.js
@@ -1,7 +1,7 @@
 export class Browser {
     constructor() {
-        this.LayoutManager = new LayoutManager();
         this.TabManager = new TabManager('tab_sortable', 1582, undefined, undefined, undefined);
+        this.LayoutManager = new LayoutManager(this.TabManager);
         this.NetworkManager = new NetworkManager();
     }
 
@@ -9,7 +9,7 @@ export class Browser {
     init() {
         try {
             this.TabManager.init();
-            //this.LayoutManager.init();
+            this.LayoutManager.init();
             //this.NetworkManager.init();
         } catch (error) {
             console.error("BrowserInstance failed to start up: " + error);
@@ -19,18 +19,24 @@ export class Browser {
 
 
 class LayoutManager {
-    constructor() {
+    constructor(tab_manager) {
+        this.tabman = tab_manager;
+    }
 
+    init() {
+        document.getElementById('tab_newtab_button').addEventListener('click', () => {
+            this.tabman.spawnTab('New Tab', true)
+        })
     }
 }
 
 class TabManager {
-    constructor(target_id, max, engineInstance = undefined, button = undefined, tabs = undefined) {
+    constructor(target_id, max, engineInstance = undefined, button = undefined) {
         this.ready = false;
         this.targetDiv = target_id;
         this.maxTabAmount = max;
         this.engine = engineInstance;
-        this.tabs = tabs;
+        this.tabs = [];
         this.initTabs = tabs;
         this.currentTab = 0;
         this.IntiatorID = button;
@@ -47,11 +53,7 @@ class TabManager {
             if (!this.tabs == undefined) {
 
             } else {
-                this.spawnTab(undefined, 'first', undefined, true);
-
-                for (let i = 1; i <= 1; i++) {
-                    this.spawnTab(undefined, `loooong text ${i}`);
-                }
+                this.spawnTab('New Tab', true);
             }
 
             Sortable.create(document.getElementById(this.targetDiv), {
@@ -84,17 +86,30 @@ class TabManager {
         return Math.floor(1000000 + Math.random() * 9000000).toString();
     }
 
-    spawnTab(state, text, icon, focused = false) {
+    spawnTab(text, focused = false) {
         if (this.ready !== true) {
             console.error('[TabManager][ERROR]: TabManager is not ready yet.');
         } else {
 
             if (this.currentAmount < this.maxTabAmount) {
                 let generatedID = this.generateRandomID();
+                while (this.tabs.includes(generatedID)) {
+                    generatedID = this.generateRandomID();
+                }
                 console.log(generatedID)
-                const newTab = this.createTabSkeleton(text, generatedID);
+                const newTab = this.createTabSkeleton(text, generatedID, focused);
+                if (focused === true) {
+                    document.querySelectorAll('.tab:not(.tab-disabled)').forEach(tab => tab.classList.add('tab-disabled'));
+                };    
                 document.getElementById(this.targetDiv).appendChild(newTab);
                 console.log('got here!');
+
+                
+                // add to registry
+                // TODO: Finish dis
+                this.tabs.push(generatedID);
+
+
                 // Attach event
                 setTimeout(() => {
                     let generatedTab = document.getElementById(generatedID);
@@ -104,9 +119,7 @@ class TabManager {
                         generatedTab.classList.remove('tab-disabled');
                     });
                     this.currentAmount++;
-                    if (focused === true) {
-                        newTab.classList.remove('tab-disabled')
-                    };    
+                    
                 }, 250);
                 
             }
@@ -114,11 +127,11 @@ class TabManager {
         }
     }
 
-    terminateTab() {
+    terminateTab(id) {
         if (this.ready !== true) {
             console.error('[TabManager][ERROR]: TabManager is not ready yet.');
         } else {
-            // close a tab logic
+            // TODO: Terminate tab based on ID
         }
     }
 
@@ -126,11 +139,14 @@ class TabManager {
         // simply closes safely, and saves the tab tree if its valid.
     }
 
-    createTabSkeleton(title, id) {
+    createTabSkeleton(title, id, focused) {
         const tab = document.createElement('sample');
         tab.setAttribute('name', 'tab');
         tab.setAttribute('title', title);
         tab.setAttribute('id', id);
+        if (focused) {
+            tab.setAttribute('name', 'tab_focused');
+        }
         return tab;
     }
 }
