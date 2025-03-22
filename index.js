d952380a44084b75d53df28c29868a3511ffb6b6
LarsIsHere <lars-private@hotmail.com>
2025-03-22 13:34:05 +0100

    beautify

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain, Menu, screen } = require('electron');
+const {app, BrowserWindow, ipcMain, Menu, screen} = require('electron');
 const path = require('node:path');
 const fs = require('node:fs');
 const userDataPath = app.getPath("userData");
@@ -9,96 +9,101 @@ Menu.setApplicationMenu(null);
 
 // Create browser window
 function createWindow() {
-  // Check if first use
-  let first_use = !fs.existsSync(path.join(userDataPath, 'user-data.json'));
-  first_use = false;
-  let primaryDisplay = screen.getPrimaryDisplay();
-  let { width, height } = primaryDisplay.workAreaSize;
+	// Check if first use
+	let first_use = !fs.existsSync(path.join(userDataPath, 'user-data.json'));
+	first_use = false;
+	let primaryDisplay = screen.getPrimaryDisplay();
+	let {
+		width,
+		height
+	} = primaryDisplay.workAreaSize;
 
-  const win = new BrowserWindow({
-    title: 'YAB',
-    show: false,
-    width: Math.floor(width * (first_use ? 0.5 : 0.7)),
-    height: Math.floor(height * (first_use ? 0.6 : 0.8)),
-    autoHideMenuBar: true,
-    titleBarStyle: 'hidden',
-    webPreferences: {
-      sandbox: false,
-      preload: path.join(__dirname, 'js', (first_use ? 'setup' : 'index'), 'preload.js')
-    }
-  })
+	const win = new BrowserWindow({
+		title: 'YAB',
+		show: false,
+		width: Math.floor(width * (first_use ? 0.5 : 0.7)),
+		height: Math.floor(height * (first_use ? 0.6 : 0.8)),
+		autoHideMenuBar: true,
+		titleBarStyle: 'hidden',
+		webPreferences: {
+			sandbox: false,
+			preload: path.join(__dirname, 'js', (first_use ? 'setup' : 'index'), 'preload.js')
+		}
+	})
 
-  if (!first_use) win.maximize();
+	if (!first_use) win.maximize();
 
-  // Doesn't seem to work
-  // TODO: Remove traffic lights in macos
-  //win.setWindowButtonVisibility(false)
+	// Doesn't seem to work
+	// TODO: Remove traffic lights in macos
+	//win.setWindowButtonVisibility(false)
 
-  if (first_use) {
-    // Setup
-    win.loadFile('pages/setup.html');
-  } else {
-    // Open normal browser
-    win.loadFile('pages/index.html');
-  }
-  win.webContents.openDevTools();
+	if (first_use) {
+		// Setup
+		win.loadFile('pages/setup.html');
+	} else {
+		// Open normal browser
+		win.loadFile('pages/index.html');
+	}
+	win.webContents.openDevTools();
 
-  function sendState() {
-    win.webContents.send('window-state', { maximized: win.isMaximized() });
-  }
-  win.on('maximize', sendState);
-  win.on('unmaximize', sendState);
+	function sendState() {
+		win.webContents.send('window-state', {
+			maximized: win.isMaximized()
+		});
+	}
+	win.on('maximize', sendState);
+	win.on('unmaximize', sendState);
 
-  win.show();
+	win.show();
 
-  ipcMain.on('spawn-process', () => {
-    const pid = Date.now(); // Unique ID for this process
-    
-    // Spawn a new process
-    childProcess.fork(path.join(__dirname,'subprocess.js'));
+	ipcMain.on('spawn-process', () => {
+		const pid = Date.now(); // Unique ID for this process
 
-    // we assign "process ids" but they arent real. there is no need to get the real windows assigned process ids. ideally a pid should be the same length as the tab ids
-    
-});
+		// Spawn a new process
+		childProcess.fork(path.join(__dirname, 'subprocess.js'));
+
+		// we assign "process ids" but they arent real. there is no need to get the real windows assigned process ids. ideally a pid should be the same length as the tab ids
+
+	});
 }
 
 app.whenReady().then(() => {
-  ipcMain.on('window-action', (event, data) => {
-    let win = BrowserWindow.fromWebContents(event.sender);
-    if (!win) return;
-    switch(data.action) {
-      case 'minimize':
-        win.minimize();
-        break;
-      case 'maximize':
-        if (win.isMaximized()) {
-          win.unmaximize();
-        } else {
-          win.maximize();
-        }
-        break;
-      case 'close':
-        win.close();
-        break;
-      case 'getwindowdata':
-        win.webContents.send('window-data', {
-          mouse: screen.getCursorScreenPoint(),
-          bounds: win.getBounds()
-        });
-        break;
-      default:
-        // Invalid action
-        break;
-    }
-  });
+	ipcMain.on('window-action', (event, data) => {
+		let win = BrowserWindow.fromWebContents(event.sender);
+		if (!win) return;
+		switch (data.action) {
+			case 'minimize':
+				win.minimize();
+				break;
+			case 'maximize':
+				if (win.isMaximized()) {
+					win.unmaximize();
+				} else {
+					win.maximize();
+				}
+				break;
+			case 'close':
+				win.close();
+				break;
+			case 'getwindowdata':
+				win.webContents.send('window-data', {
+					mouse: screen.getCursorScreenPoint(),
+					bounds: win.getBounds()
+				});
+				break;
+			default:
+				// Invalid action
+				break;
+		}
+	});
 
-  createWindow()
+	createWindow()
 })
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
+	if (process.platform !== 'darwin') app.quit()
 })
 
 ipcMain.on('close-request', () => {
-  app.quit();
+	app.quit();
 });
--- js/browserComponents.js
+++ js/browserComponents.js
@@ -1,240 +1,240 @@
 export class Browser {
-    constructor(engine) {
-        this.TabManager = new TabManager('tab_sortable', 1582, engine, this.terminateBrowserInstance_safe);
-        this.LayoutManager = new LayoutManager(this.TabManager);
-        this.NetworkManager = new NetworkManager();
-    }
+	constructor(engine) {
+		this.TabManager = new TabManager('tab_sortable', 1582, engine, this.terminateBrowserInstance_safe);
+		this.LayoutManager = new LayoutManager(this.TabManager);
+		this.NetworkManager = new NetworkManager();
+	}
 
 
-    init() {
-        try {
-            this.TabManager.init();
-            this.LayoutManager.init();
-            //this.NetworkManager.init();
-        } catch (error) {
-            throw new Error(error);
-            console.error("BrowserInstance failed to start up: " + error);
+	init() {
+		try {
+			this.TabManager.init();
+			this.LayoutManager.init();
+			//this.NetworkManager.init();
+		} catch (error) {
+			throw new Error(error);
+			console.error("BrowserInstance failed to start up: " + error);
 
-        }
-    }
+		}
+	}
 
-    terminateBrowserInstance_safe() {
-        // here it would do all the stuff it needs to do like ending lua vm/subprocesses etc.
-        closeApp.close();
-    }
+	terminateBrowserInstance_safe() {
+		// here it would do all the stuff it needs to do like ending lua vm/subprocesses etc.
+		closeApp.close();
+	}
 
-    killBrowserInstance() {
-        // just kill the app
-        closeApp.close();
-    }
+	killBrowserInstance() {
+		// just kill the app
+		closeApp.close();
+	}
 }
 
 
 class LayoutManager {
-    constructor(tab_manager) {
-        this.tabman = tab_manager;
-    }
+	constructor(tab_manager) {
+		this.tabman = tab_manager;
+	}
 
-    init() {
-        document.getElementById('tab_newtab_button').addEventListener('click', () => {
-            this.tabman.spawnTab('New Tab', true)
-        })
-    }
+	init() {
+		document.getElementById('tab_newtab_button').addEventListener('click', () => {
+			this.tabman.spawnTab('New Tab', true)
+		})
+	}
 }
 
 class TabManager {
-    constructor(target_id, max, engineInstance, BrowserInstance) {
-        this.ready = false;
-        this.targetDiv = target_id;
-        this.maxTabAmount = max;
-        this.engine = engineInstance;
-        this.tabs = [];
-        this.initTabs = this.tabs;
-        this.currentTab = 0;
-        this.currentAmount = 0;
-        this.browserTerminate = BrowserInstance;
-    }
+	constructor(target_id, max, engineInstance, BrowserInstance) {
+		this.ready = false;
+		this.targetDiv = target_id;
+		this.maxTabAmount = max;
+		this.engine = engineInstance;
+		this.tabs = [];
+		this.initTabs = this.tabs;
+		this.currentTab = 0;
+		this.currentAmount = 0;
+		this.browserTerminate = BrowserInstance;
+	}
 
-    init() {
-        if (!document.getElementById(this.targetDiv)) {
-            console.error('[TabManager][FATAL]: Cannot initialize. Target Tab Container "' + this.targetDiv + '" does not exist.');
-            throw new Error();
-        } else {
-            this.ready = true;
+	init() {
+		if (!document.getElementById(this.targetDiv)) {
+			console.error('[TabManager][FATAL]: Cannot initialize. Target Tab Container "' + this.targetDiv + '" does not exist.');
+			throw new Error();
+		} else {
+			this.ready = true;
 
-            if (!this.tabs == undefined) {
+			if (!this.tabs == undefined) {
 
-            } else {
-                this.spawnTab('New Tab', true);
-            }
+			} else {
+				this.spawnTab('New Tab', true);
+			}
 
-            Sortable.create(document.getElementById(this.targetDiv), {
-                swapThreshold: 0.90,
-                animation: 150,
-                preventOnFilter: true,
-                preventOnCancel: false,
-                easing: "cubic-bezier(1, 0, 0, 1)",
-                onEnd: function() {
-                    backend.getWindowData().then(data => {
-                        let mouse = data.mouse;
-                        let bounds = data.bounds; //document.getElementById('titlebar').getBoundingClientRect()
+			Sortable.create(document.getElementById(this.targetDiv), {
+				swapThreshold: 0.90,
+				animation: 150,
+				preventOnFilter: true,
+				preventOnCancel: false,
+				easing: "cubic-bezier(1, 0, 0, 1)",
+				onEnd: function() {
+					backend.getWindowData().then(data => {
+						let mouse = data.mouse;
+						let bounds = data.bounds; //document.getElementById('titlebar').getBoundingClientRect()
 
-                        const isInsideWindow =
-                            mouse.x >= bounds.x &&
-                            mouse.x <= bounds.x + bounds.width &&
-                            mouse.y >= bounds.y &&
-                            mouse.y <= bounds.y + bounds.height;
+						const isInsideWindow =
+							mouse.x >= bounds.x &&
+							mouse.x <= bounds.x + bounds.width &&
+							mouse.y >= bounds.y &&
+							mouse.y <= bounds.y + bounds.height;
 
-                        console.log(`Is Inside Window: ${isInsideWindow}`);
-                    })
-                }
-            });
+						console.log(`Is Inside Window: ${isInsideWindow}`);
+					})
+				}
+			});
 
-            return;
-        }
-    }
+			return;
+		}
+	}
 
-    generateRandomID() {
-        return Math.floor(1000000 + Math.random() * 9000000).toString();
-    }
+	generateRandomID() {
+		return Math.floor(1000000 + Math.random() * 9000000).toString();
+	}
 
-    spawnTab(text, focused = false) {
-        if (this.ready !== true) {
-            console.error('[TabManager][ERROR]: TabManager is not ready yet.');
-        } else {
+	spawnTab(text, focused = false) {
+		if (this.ready !== true) {
+			console.error('[TabManager][ERROR]: TabManager is not ready yet.');
+		} else {
 
-            if (this.currentAmount < this.maxTabAmount) {
-                let generatedID = this.generateRandomID();
-                while (this.tabs.includes(generatedID)) {
-                    generatedID = this.generateRandomID();
-                }
-                console.log(generatedID)
-                console.log(CitronJS.getContent);
-                const newTab = this.createTabSkeleton(text, generatedID, focused);
-                console.log(newTab);
-                if (focused === true) {
-                    document.querySelectorAll('.tab:not(.tab-disabled)').forEach(tab => tab.classList.add('tab-disabled'));
+			if (this.currentAmount < this.maxTabAmount) {
+				let generatedID = this.generateRandomID();
+				while (this.tabs.includes(generatedID)) {
+					generatedID = this.generateRandomID();
+				}
+				console.log(generatedID)
+				console.log(CitronJS.getContent);
+				const newTab = this.createTabSkeleton(text, generatedID, focused);
+				console.log(newTab);
+				if (focused === true) {
+					document.querySelectorAll('.tab:not(.tab-disabled)').forEach(tab => tab.classList.add('tab-disabled'));
 
-                };
-                document.getElementById(this.targetDiv).appendChild(newTab);
-                console.log('got here!');
-
-
-                // add to registry
-                // TODO: Finish dis
-                this.tabs.push(generatedID);
-                this.currentTab = generatedID;
-
-                this.engine.spawnNewProcess();
-                // Attach event
-
-                let generatedTab = document.getElementById(generatedID);
-                console.log(generatedTab)
-                generatedTab.querySelector('.tab_left').addEventListener('mousedown', () => {
-                    document.querySelectorAll('.tab:not(.tab-disabled)').forEach(tab => tab.classList.add('tab-disabled'));
-                    generatedTab.classList.remove('tab-disabled');
-                    this.currentTab = generatedID;
-                });
-                generatedTab.querySelector('.tab_close_container').addEventListener('click', () => {
-                    this.terminateTab(generatedID);
-                });
-                this.currentAmount++;
+				};
+				document.getElementById(this.targetDiv).appendChild(newTab);
+				console.log('got here!');
 
 
-            }
+				// add to registry
+				// TODO: Finish dis
+				this.tabs.push(generatedID);
+				this.currentTab = generatedID;
 
-        }
-    }
+				this.engine.spawnNewProcess();
+				// Attach event
 
-    terminateTab(id) {
-        if (this.ready !== true) {
-            console.error('[TabManager][ERROR]: TabManager is not ready yet.');
-        } else {
-            // TODO: Handle closing a focused tab
-            // Find and remove tab entry
-            const container = document.getElementById(this.targetDiv); // or document.getElementById('myDiv')
-            const currentElem = document.getElementById(id); // or however you determine it
-            // Remove tab element
-            const lastChild = container.lastElementChild;
-            console.log(lastChild);
-            let neighbour = null;
+				let generatedTab = document.getElementById(generatedID);
+				console.log(generatedTab)
+				generatedTab.querySelector('.tab_left').addEventListener('mousedown', () => {
+					document.querySelectorAll('.tab:not(.tab-disabled)').forEach(tab => tab.classList.add('tab-disabled'));
+					generatedTab.classList.remove('tab-disabled');
+					this.currentTab = generatedID;
+				});
+				generatedTab.querySelector('.tab_close_container').addEventListener('click', () => {
+					this.terminateTab(generatedID);
+				});
+				this.currentAmount++;
 
-            if (currentElem === lastChild) {
-                // If the current element is the last child, get its previous sibling (to the left)
-                neighbour = currentElem.previousElementSibling;
-            } else {
-                // currentElem is not the last child of the DIV.
-                if (currentElem !== lastChild && !this.tabs.includes(lastChild.id)) {
-                    // If the container's last child is NOT in the list, get currentElem's previous sibling
-                    neighbour = currentElem.previousElementSibling;
-                } else {
-                    // If the container's last child IS in the list, get currentElem's next sibling (to the right)
-                    neighbour = currentElem.nextElementSibling;
-                }
-            }
-            let idx = this.tabs.indexOf(id);
-            this.tabs.splice(idx, 1);
 
-            this.currentAmount--;
+			}
 
-            let tabtodel = document.getElementById(id)
+		}
+	}
 
-            tabtodel.style.width = '200px';
-            tabtodel.classList.remove('tab_animation');
-            tabtodel.classList.add('tab_animation-close');
+	terminateTab(id) {
+		if (this.ready !== true) {
+			console.error('[TabManager][ERROR]: TabManager is not ready yet.');
+		} else {
+			// TODO: Handle closing a focused tab
+			// Find and remove tab entry
+			const container = document.getElementById(this.targetDiv); // or document.getElementById('myDiv')
+			const currentElem = document.getElementById(id); // or however you determine it
+			// Remove tab element
+			const lastChild = container.lastElementChild;
+			console.log(lastChild);
+			let neighbour = null;
 
-            setTimeout(() => {
+			if (currentElem === lastChild) {
+				// If the current element is the last child, get its previous sibling (to the left)
+				neighbour = currentElem.previousElementSibling;
+			} else {
+				// currentElem is not the last child of the DIV.
+				if (currentElem !== lastChild && !this.tabs.includes(lastChild.id)) {
+					// If the container's last child is NOT in the list, get currentElem's previous sibling
+					neighbour = currentElem.previousElementSibling;
+				} else {
+					// If the container's last child IS in the list, get currentElem's next sibling (to the right)
+					neighbour = currentElem.nextElementSibling;
+				}
+			}
+			let idx = this.tabs.indexOf(id);
+			this.tabs.splice(idx, 1);
 
-                tabtodel.remove();
-                console.log(this.tabs.length);
-                console.log("it is " + this.tabs.length === 0);
-            }, 220);
-            if (this.tabs.length > 0) {
-                console.log('hallo?');
-                if (this.currentTab === id) {
-                    this.setFocus(neighbour.id);
-                    console.log(neighbour);
-                }
+			this.currentAmount--;
 
-            }
-            if (this.tabs.length === 0) {
-                this.browserTerminate();
-            }
+			let tabtodel = document.getElementById(id)
 
-        }
-    }
+			tabtodel.style.width = '200px';
+			tabtodel.classList.remove('tab_animation');
+			tabtodel.classList.add('tab_animation-close');
 
-    setFocus(id) {
-        this.currentTab = id;
-        document.querySelectorAll('.tab:not(.tab-disabled)').forEach(tab => tab.classList.add('tab-disabled'));
-        document.getElementById(id).classList.remove('tab-disabled');
+			setTimeout(() => {
 
-    }
+				tabtodel.remove();
+				console.log(this.tabs.length);
+				console.log("it is " + this.tabs.length === 0);
+			}, 220);
+			if (this.tabs.length > 0) {
+				console.log('hallo?');
+				if (this.currentTab === id) {
+					this.setFocus(neighbour.id);
+					console.log(neighbour);
+				}
 
-    cleanup() {
-        // simply closes safely, and saves the tab tree if its valid.
-    }
+			}
+			if (this.tabs.length === 0) {
+				this.browserTerminate();
+			}
 
-    createTabSkeleton(title, id, focused) {
-        if (focused) {
-            return CitronJS.getContent('tab_focused', {
-                title: title,
-                id: id
-            });
-        } else {
-            return CitronJS.getContent('tab', {
-                title: title,
-                id: id
-            });
-        }
-    }
+		}
+	}
+
+	setFocus(id) {
+		this.currentTab = id;
+		document.querySelectorAll('.tab:not(.tab-disabled)').forEach(tab => tab.classList.add('tab-disabled'));
+		document.getElementById(id).classList.remove('tab-disabled');
+
+	}
+
+	cleanup() {
+		// simply closes safely, and saves the tab tree if its valid.
+	}
+
+	createTabSkeleton(title, id, focused) {
+		if (focused) {
+			return CitronJS.getContent('tab_focused', {
+				title: title,
+				id: id
+			});
+		} else {
+			return CitronJS.getContent('tab', {
+				title: title,
+				id: id
+			});
+		}
+	}
 }
 
 class NetworkManager {
-    constructor() {
+	constructor() {
 
-    }
-    init() {
+	}
+	init() {
 
-    }
+	}
 }
